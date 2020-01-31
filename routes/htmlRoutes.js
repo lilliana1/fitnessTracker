3f0abe270292ec9ce161ccda0cd0b7293e5a8353
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-01-30 19:34:01 -0500

    setting html routes

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,13 @@
+// GET method route
+app.get('/', function (req, res) {
+    res.send('')
+})
+
+app.get('/exercise', function (req, res) {
+    res.send('')
+})
+
+app.get('/stats', function (req, res) {
+    res.send('')
+})
+
