35c42577751acb36cbd520fc62bbca175d22b748
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-02-01 12:09:31 -0500

    added some relevant comments

--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,14 +1,18 @@
 module.exports = function(app) {
 
 // GET method route
+
+// loads index.html
 app.get('/', function (req, res) {
     res.sendFile(__dirname + '/public/index')
 })
 
+// loads exercise.html
 app.get('/exercise', function (req, res) {
     res.sendFile(__dirname + '/public/exercise')
 })
 
+// loads stats.html
 app.get('/stats', function (req, res) {
     res.sendFile(__dirname + '/public/stats')
 })
