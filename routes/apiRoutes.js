8418d7a492a3675284e3937a7a6f45119cd994cc
Lilliana Ramos <lilliana.cramos@gmail.com>
2020-02-03 17:05:38 -0500

    html routes are now working, i was missing declaring path and instead of +  was ,

--- routes/apiRoutes.js
+++ routes/apiRoutes.js
@@ -1,98 +1,72 @@
 const db = require("../models")
 
+// Exporting API routes
 module.exports = function(app) {
-
 
 // Read all documents from the database.
 app.get("/api/workouts", (req, res) => {
-    db.Workout.find({})
-    .then(dbData => {
-        console.log(dbData);
-        res.json(dbData);
-    })
-    .catch(err => {
-        res.json(err)
-    });
+    db.Workout.find({}, err, data)
+    if (err)
+    {
+        res.send(err)
+    } else {
+        res.json(data)
+    }
 });
+
 // Create a Workout Document
-app.post("/api/workouts", ({body}, res) => {
-db.Workout.create(body)
-    .then(({name}) => db.Workout.findOneAndUpdate({}, { $push: { exercises: name } }, { new: true }))
-    .then(dbWorkout => {
-    res.json(dbWorkout);
-    })
-    .catch(err => {
-    res.json(err);
-    });
+app.post("/api/workouts", (req, res) => {
+    db.Workout.create(req.body, (err, data))
+    if (err) {
+        res.send(err)
+    } else {
+        res.send(data)
+    }
+    
 
 // Update (add) exercises to the workout document.
-
-
-
+app.post("/api/workouts/:id", (req, res) => {
+    db.Workout.update(
+        {
+            _id: mongojs.ObjectId(req.params.id)
+        },
+        {
+            $set:
+            {
+                type: req.body.type,
+                name: req.body.name,
+                duration: req.body.duration,
+                weight: req.body.weight,
+                reps: req.body.reps,
+                sets: req.body.sets,
+                distance: req.body.distance
+            }
+        },
+        (err, data) =>
+        {
+            if (err)
+            {
+                res.send(err);
+            } else {
+                res.send(data);
+            }
+        }
+    );
 
 // Read the last 7 documents (Workouts) from the collection to be displayed in the "stats.html" page
-
-
+ app.get("/api/workouts/range", (req, res) => {
+    db.Workout.find({}, (err, data) => {
+        if (err)
+        {
+            res.send(err);
+        } else {
+            res.json(data);
+        }
+    }).limit(7);
+});
 
 });
 
-// app.get("/books", (req, res) => {
-// db.Book.find({})
-//   .then(dbBook => {
-//     res.json(dbBook);
-//   })
-//   .catch(err => {
-//     res.json(err);
-//   });
-// });
+});
 
-// db.Library.create({ name: "Campus Library" })
-// .then(dbLibrary => {
-//   console.log(dbLibrary);
-// })
-// .catch(({message}) => {
-//   console.log(message);
-// });
-
-// app.post("/submit", ({body}, res) => {
-// db.Book.create(body)
-//   .then(({_id}) => db.Library.findOneAndUpdate({}, { $push: { books: _id } }, { new: true }))
-//   .then(dbLibrary => {
-//     res.json(dbLibrary);
-//   })
-//   .catch(err => {
-//     res.json(err);
-//   });
-// });
-
-// app.get("/books", (req, res) => {
-// db.Book.find({})
-//   .then(dbBook => {
-//     res.json(dbBook);
-//   })
-//   .catch(err => {
-//     res.json(err);
-//   });
-// });
-
-// app.get("/library", (req, res) => {
-// db.Library.find({})
-//   .then(dbLibrary => {
-//     res.json(dbLibrary);
-//   })
-//   .catch(err => {
-//     res.json(err);
-//   });
-// });
-
-// app.get("/populated", (req, res) => {
-// db.Library.find({})
-//   .populate("books")
-//   .then(dbLibrary => {
-//     res.json(dbLibrary);
-//   })
-//   .catch(err => {
-//     res.json(err);
-//   });
-// });
 }
--- routes/htmlRoutes.js
+++ routes/htmlRoutes.js
@@ -1,20 +1,21 @@
+const path = require("path");
+
 module.exports = function(app) {
 
 // GET method route
 
 // loads index.html
-app.get('/', function (req, res) {
-    res.sendFile(__dirname + './public/index.html')
+app.get("/", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/index.html"))
 })
 
 // loads exercise.html
-app.get('/exercise', function (req, res) {
-    res.sendFile(__dirname + './public/exercise.html')
+app.get("/exercise", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/exercise.html"))
 })
 
 // loads stats.html
-app.get('/stats', function (req, res) {
-    res.sendFile(__dirname + './public/stats.html')
+app.get("/stats", function (req, res) {
+    res.sendFile(path.join(__dirname, "../public/stats.html"))
 })
-
-}
+};
